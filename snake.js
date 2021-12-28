6981c93651cd16fd1db1744e70e7f4989c222c83
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-12-27 19:38:48 -0500

    finished game

--- snake.js
+++ snake.js
@@ -60,10 +60,10 @@ class Snake{
                 this.body.push([this.body[this.body.length - 1][0] + 1, this.body[this.body.length - 1][1], this.body[this.body.length - 1][2]])
                 break
             case "up":
-                this.body.push([this.body[this.body.length - 1][0], this.body[this.body.length - 1][1] - 1, this.body[this.body.length - 1][2]])
+                this.body.push([this.body[this.body.length - 1][0], this.body[this.body.length - 1][1] + 1, this.body[this.body.length - 1][2]])
                 break
             case "down":
-                this.body.push([this.body[this.body.length - 1][0], this.body[this.body.length - 1][1] + 1, this.body[this.body.length - 1][2]])
+                this.body.push([this.body[this.body.length - 1][0], this.body[this.body.length - 1][1] - 1, this.body[this.body.length - 1][2]])
                 break
         }
     }
@@ -79,6 +79,14 @@ class Snake{
             this.body[i][2] = this.body[i - 1][2]
         }
     }
+
+    checkCollision(){
+        for(let i = 1; i < this.body.length; i++){
+            if(this.body[i][0] == this.body[0][0] && this.body[i][1] == this.body[0][1]) return true
+        }
+
+        return false
+    }
 }
 
 class Food{
@@ -87,9 +95,12 @@ class Food{
         this.y = y
     }
 
-    eat(){
+    eat(snake){
         this.x = Math.floor(Math.random() * 16)
         this.y = Math.floor(Math.random() * 16)
+        for(let i = 0; i < snake.body.length; i++){
+            while (snake.body[i][0] == this.x && snake.body[i][1] == this.y) this.eat(snake)
+        }
     }
 }
 
@@ -138,6 +149,7 @@ class Board{
             snake.move()
             this.refresh(snake, food)
             snake.refreshDirection()
+            if(snake.checkCollision()) this.lose()
         }, this.tickSpeed)
     }
 
@@ -165,17 +177,10 @@ class Board{
         if(snake.body[0][0] == food.x && snake.body[0][1] == food.y){
             console.log("food eaten")
             snake.increaseLength()
-            food.eat()
+            food.eat(snake)
         }
 
-        for(let i = 0; i < 17; i++){
-            for(let j = 0; j < 17; j++){
-                let cell = document.getElementsByClassName(i + "," + j)[0]
-                if(this.gameBoard[i][j].contents == "empty") cell.style.backgroundColor = "black"
-                else if( this.gameBoard[i][j].contents == "snake") cell.style.backgroundColor = "green"
-                else if( this.gameBoard[i][j].contents == "food") cell.style.backgroundColor = "red"
-            }
-        }
+        this.render()
     }
 
     clear(){
@@ -186,9 +191,30 @@ class Board{
         }
     }
 
-    lose(){
-        console.log("you lose")
+    async lose(){
         clearInterval(this.interval)
+        this.clear()
+        let youWord = [[2,2],[2,3],[3,4],[3,5],[4,3],[4,2], [6,2],[6,3],[6,4],[7,2],[7,5],[8,2],[8,3],[8,4],[8,5],[6,5], [10,2],[10,3],[10,4],[10,5],[11,5],[12,5],[12,4],[12,3],[12,2]]
+        let loseWord = [[2,8],[2,9],[2,10],[2,11],[2,12],[3,12],[4,12], [6,8],[6,9],[6,10],[6,11],[6,12],[7,12],[7,8],[8,8],[8,12],[8,9],[8,10],[8,11],[12, 8],[11, 8],[10, 8],[10, 9],[10, 10],[11, 10],[12, 10],[12, 11],[12, 12],[11, 12],[10, 12],[15, 8],[14, 8], [16,8], [14,9],[14,10],[14,11],[14,12],[15,10],[16,10],[15,12],[16,12]]
+        for(let i = 0; i < youWord.length; i++){
+            this.gameBoard[youWord[i][1]][youWord[i][0] + 1].contents = "endscreen"
+        }
+        for(let i = 0; i < loseWord.length; i++){
+            this.gameBoard[loseWord[i][1] + 1][loseWord[i][0] - 1].contents = "endscreen"
+        }
+        this.render()
+    }
+
+    render(){
+        for(let i = 0; i < 17; i++){
+            for(let j = 0; j < 17; j++){
+                let cell = document.getElementsByClassName(i + "," + j)[0]
+                if(this.gameBoard[i][j].contents == "empty") cell.style.backgroundColor = "black"
+                else if( this.gameBoard[i][j].contents == "snake") cell.style.backgroundColor = "green"
+                else if( this.gameBoard[i][j].contents == "food") cell.style.backgroundColor = "red"
+                else if( this.gameBoard[i][j].contents == "endscreen") cell.style.backgroundColor = "yellow"
+            }
+        }
     }
 }
 
@@ -214,6 +240,8 @@ async function addKeyListeners(snake, board){
                 snake.changeDirection("down")
                 previousBoardTick = board.tick
                 break
+            case 32:
+                location.reload()
         }
     })
 }
